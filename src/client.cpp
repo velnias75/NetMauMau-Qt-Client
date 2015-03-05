b7277917f7687ecd47481cc3a8f0f1a31e6ca29c
velnias75 <heiko@rangun.de>
2015-03-05 13:15:47 +0100

    bumped internal client version to 16

--- src/client.cpp
+++ src/client.cpp
@@ -32,7 +32,7 @@
 #include "mainwindow.h"
 #include "base64bridge.h"
 
-#define CLIENTVERSION 15
+#define CLIENTVERSION 16
 
 Client::Client(MainWindow *const w, ConnectionLogDialog *cld, const QString &player,
 			   const std::string &server, uint16_t port) : QThread(),
